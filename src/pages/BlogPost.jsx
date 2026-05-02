81dafaea00ca9c6225fefcf3e7489844478f4379
Vladko Coder <vladko13111999@gmail.com>
2026-05-02 10:27:35 +0000

    Fix: Move useEffect import to react

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,4 +1,5 @@
-import { useParams, useEffect, Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import blogPosts from '../data/blogPosts';
 import AdSenseSlots from '../components/AdSenseSlots';
 
