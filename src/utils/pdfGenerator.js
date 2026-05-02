313b52ca2ae3a2c898fbebeb91ae22bb9524e306
Vladko Coder <vladko13111999@gmail.com>
2026-05-02 10:36:04 +0000

    Remove all old files

--- src/utils/pdfGenerator.js
+++
@@ -1,5 +0,0 @@
-// Simplified PDF generator - placeholder since jspdf has issues with Vite 8
-export const generateBrandKitPDF = (brandData) => {
-  alert('PDF export bude k dispozícii čoskoro! Teraz si môžete skopírovať farby a fonty.');
-  return null;
-};
